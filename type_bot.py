263a12a1e99df341c6b36eb15e21f55013b006fb
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-20 15:56:09 -0700

    Add files via upload

---
+++ type_bot.py
@@ -0,0 +1,14 @@
+import datetime
+
+#type bot:
+
+#5 secs after init, starts typing
+#1 character slowwly
+#for 30 seconds
+#webbrowser opens to google with type words
+#program ends after 30 seconds
+
+#type certain things depending on min of day
+#type certain things depending on hour of day
+#type certain things depending on month of day
+#type certain things depending on year of day
