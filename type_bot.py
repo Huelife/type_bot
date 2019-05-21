5791afc70c94466fabe19db7f517113c23286110
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-21 16:30:00 -0700

    Update type_bot.py

--- type_bot.py
+++ type_bot.py
@@ -2,39 +2,48 @@
 import time
 import datetime
 import webbrowser
+import sys
+
+now = datetime.datetime.now()
+datetime.time(now.minute,now.hour,now.day)
+now_min = now.minute
+now_hr = now.hour
+now_day = now.day
+now_mnth = 0
+now_yr = 0
 
 url = 'http://lmgtfy.com/?q=zerg+rush'
 
 chrome_loc = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
 
-def msg_type():
-  msg = " "
-  print(msg)
-  if now_min == # :
-    msg = " "
-  elif now_hr == # :
-    msg = " "
-  elif now_day == # :
-    msg = " "
-  elif now_mnth == # :
-    msg = " "
-  elif now_yr == # :
-    msg = " "
-  else:
-    msg = "ERROR!"
 #type certain things depending on min of day
 #type certain things depending on hour of day
 #type certain things depending on month of day
 #type certain things depending on year of day
+def msg_type():
+  msg = "Cheeese"
+  print(msg)
+  if now_min == 9:
+    msg = "oh"
+  elif now_hr == 12:
+    msg = "hi"
+  elif now_day == 12:
+    msg = "yuh"
+  elif now_mnth == 12:
+    msg = "there"
+  elif now_yr == 2020:
+    msg = "buddy"
+  else:
+    msg = "ERROR!"
 
-time.sleep(5) #5 secs after init, starts typing
+time.sleep(1.5) #1.5 secs after init, starts typing
 
-msg_type() #1 character slowwly
+msg_type() #1 character slowly
 
-time.sleep(30) #30 seconds
+time.sleep(5) #5 seconds
 
-#webbrowser opens to google with type words
+#webbrowser opens to let me google that for you: zerg rush
 webbrowser.get(chrome_loc).open_new_tab(url)
-#program ends after 30 seconds
 
-
+#program ends after 6.5 seconds
+sys.exit(0)
