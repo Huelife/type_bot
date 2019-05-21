bdaeb6e868d017abb5da9a9bf2b1495bd6d10ecf
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-21 10:08:53 -0700

    Update type_bot.py

--- type_bot.py
+++ type_bot.py
@@ -1,11 +1,16 @@
 import time
+import webbrowser
+
+url = 'http://lmgtfy.com/?q=zerg+rush'
+
+chrome_loc = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
 
 #type bot:
 
 #time.sleep(5) 5 secs after init, starts typing
 #1 character slowwly
 #time.sleep(30) 30 seconds
-#webbrowser opens to google with type words
+#webbrowser.get(chrome_loc).open_new_tab(url) webbrowser opens to google with type words
 #program ends after 30 seconds
 
 #type certain things depending on min of day
