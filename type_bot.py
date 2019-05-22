bba7790ed78d498de4abbe66b951a1c4e5336403
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-21 22:11:47 -0700

    Update type_bot.py

--- type_bot.py
+++ type_bot.py
@@ -5,45 +5,44 @@ import webbrowser
 import sys
 
 now = datetime.datetime.now()
-datetime.time(now.minute,now.hour,now.day)
-now_min = now.minute
+datetime.time(now.hour)
 now_hr = now.hour
-now_day = now.day
-now_mnth = 0
-now_yr = 0
 
 url = 'http://lmgtfy.com/?q=zerg+rush'
 
 chrome_loc = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
 
-#type certain things depending on min of day
 #type certain things depending on hour of day
-#type certain things depending on month of day
-#type certain things depending on year of day
-def msg_type():
-  msg = "Cheeese"
-  print(msg)
-  if now_min == 9:
-    msg = "oh"
-  elif now_hr == 12:
-    msg = "hi"
-  elif now_day == 12:
-    msg = "yuh"
-  elif now_mnth == 12:
-    msg = "there"
-  elif now_yr == 2020:
-    msg = "buddy"
+def msg_type(): 
+  if now_hr >= 0 and now_hr < 1:
+    msg = iter("Don't look under your bed...")
+  elif now_hr >= 1 and now_hr < 6:
+    msg = iter("Have fun in your dreams...")
+  elif now_hr >= 6 and now_hr < 12:
+    msg = iter("WHY AM I INSIDE THIS COMPUTER!?")
+  elif now_hr >= 12 and now_hr < 18:
+    msg = iter("HELP! I'M STUCK INSIDE!!")
+  elif now_hr >= 18 and now_hr < 24:
+    msg = iter("Don't turn on the lights...")
   else:
-    msg = "ERROR!"
+    msg = iter("WE'RE SINKING!!")
+    
+  sys.stdout.write(next(msg))
+  sys.stdout.flush()
 
-time.sleep(1.5) #1.5 secs after init, starts typing
+  for i in msg:
+    time.sleep(0.25)
+    sys.stdout.write(i)
+    sys.stdout.flush()
+  
+time.sleep(1.5) #1.5 secs after init, start typing
 
-msg_type() #1 character slowly
+msg_type()#1 character slowly
 
-time.sleep(5) #5 seconds
+time.sleep(1.5) #1.5 secs after typing, open chrome
 
-#webbrowser opens to let me google that for you: zerg rush
+#webbrowser opens to 'let me google that for you'(lmgtfy): zerg rush
 webbrowser.get(chrome_loc).open_new_tab(url)
 
-#program ends after 6.5 seconds
+#program ends after a few seconds
 sys.exit(0)
