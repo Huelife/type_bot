dadcef305bc43790db938e15c65aa86d70482d21
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-22 00:02:32 -0700

    Update type_bot.py
    removed now_hr and just used now.hour since it seemed repetitive, also added and moved comments around

--- type_bot.py
+++ type_bot.py
@@ -6,22 +6,21 @@ import sys
 
 now = datetime.datetime.now()
 datetime.time(now.hour)
-now_hr = now.hour
 
 url = 'http://lmgtfy.com/?q=zerg+rush'
 chrome_loc = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
 
 #type certain things depending on hour of day
 def msg_type(): 
-  if now_hr >= 0 and now_hr < 1:
+  if now.hour >= 0 and now.hour < 1:
     msg = iter("Don't look under your bed...")
-  elif now_hr >= 1 and now_hr < 6:
+  elif now.hour >= 1 and now.hour < 6:
     msg = iter("Have fun in your dreams...")
-  elif now_hr >= 6 and now_hr < 12:
+  elif now.hour >= 6 and now.hour < 12:
     msg = iter("WHY AM I INSIDE THIS COMPUTER!?")
-  elif now_hr >= 12 and now_hr < 18:
+  elif now.hour >= 12 and now.hour < 18:
     msg = iter("HELP! I'M STUCK INSIDE!!")
-  elif now_hr >= 18 and now_hr < 24:
+  elif now.hour >= 18 and now.hour < 24:
     msg = iter("Don't turn on the lights...")
   else:
     msg = iter("WE'RE SINKING!!")
@@ -33,13 +32,13 @@ def msg_type():
     time.sleep(0.25)
     sys.stdout.write(i)
     sys.stdout.flush()
-  
-time.sleep(1.5) #1.5 secs after init, start typing
-msg_type()#1 character slowly
-time.sleep(1.5) #1.5 secs after typing, open chrome
+ 
+#wait 1.5 sec, print message, and wait 1.5 sec
+time.sleep(1.5)
+msg_type()
+time.sleep(1.5)
 
 #webbrowser opens to 'let me google that for you'(lmgtfy): zerg rush
-webbrowser.get(chrome_loc).open_new_tab(url)
-
 #program ends after a few seconds
+webbrowser.get(chrome_loc).open_new_tab(url)
 sys.exit(0)
