0320b7606b80e519cf3ac6333b82e71216e19f75
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-22 19:01:13 -0700

    Update type_bot.py
    program wont function unless I move import browser and browser information to msg so I moved them

--- type_bot.py
+++ type_bot.py
@@ -1,17 +1,11 @@
-#type bot:
-import time
-import webbrowser
-import sys
+#type bot: types a message and opens chrome
 
-url = 'http://lmgtfy.com/?q=zerg+rush'
-chrome_loc = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+import time
+import sys
  
 #wait 1.5 sec, print message, and wait 1.5 sec
 time.sleep(1.5)
 import msg.py
-time.sleep(1.5)
 
-#webbrowser opens to 'let me google that for you'(lmgtfy): zerg rush
 #program ends after a few seconds
-webbrowser.get(chrome_loc).open_new_tab(url)
 sys.exit(0)
