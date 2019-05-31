25e271ae0d39540d617a158862791ebb69c6abef
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-31 01:46:39 -0700

    Update thursday.py
    added message iteration and changed m to msg_list

--- thursday.py
+++ thursday.py
@@ -8,29 +8,38 @@ now = datetime.datetime.now()
 datetime.time(now.hour)
 
 def thursday_msg():
-  m = ["a0",
-       "a1",
-       "a2",
-       "a3",
-       "a4",
-       "a5",
-       "a6",
-       "a7",
-       "a8",
-       "a9",
-       "a10",
-       "a11",
-       "a12",
-       "a13",
-       "a14",
-       "a15",
-       "a16",
-       "a17",
-       "a18",
-       "a19",
-       "a20",
-       "a21",
-       "a22",
-       "a23"]
+  msg_list = ["a0",
+              "a1",
+              "a2",
+              "a3",
+              "a4",
+              "a5",
+              "a6",
+              "a7",
+              "a8",
+              "a9",
+              "a10",
+              "a11",
+              "a12",
+              "a13",
+              "a14",
+              "a15",
+              "a16",
+              "a17",
+              "a18",
+              "a19",
+              "a20",
+              "a21",
+              "a22",
+              "a23"]
+
+  msg = iter(msg_list[now.hour])
+  sys.stdout.write(next(msg))
+  sys.stdout.flush()
+
+  for i in msg:
+    time.sleep(0.25)
+    sys.stdout.write(i)
+    sys.stdout.flush()
     
 thursday_msg()
